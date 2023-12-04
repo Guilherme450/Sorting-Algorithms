315988f84b9eb7c84807bac8c5e2e0ce1bae400a
Guilherme Lima <guilherme.l450@protonmail.com>
2023-12-04 11:49:04 -0300

    bubble sort algorithm 1.0

---
+++ bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(array):
+    n = len(array)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
+
+array = [6,8,7,2,9,1,10,3]
+print(bubble_sort(array))
